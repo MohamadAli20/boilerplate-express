ce825b4d89a6b7096fc6539e55bc24c89ab11c2b
Mohamad Ali Calanda <135398263+MohamadAli20@users.noreply.github.com>
2024-01-02 19:57:25 +0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,43 +1,8 @@
-let express = require('express'); 
+let express = require("express");
 let app = express();
 
-app.get('/', function(req, res){
- res.send("Hello World");
+app.get("/", function (req, res) {
+  const absolutePath = __dirname + "/views/index.html";
+  res.sendFile(absolutePath);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
